d5f6c76f7cef29a3a834af10780c403ce5fe33de
Guilherme Borges <guilhermebborges04m2@gmail.com>
2022-09-16 10:19:50 -0300

    ewe

--- Hello_World01/src/Main.java
+++ Hello_World01/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String nome = "Guilherme";
+        int idade = 21;
+        int a, b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+
+        System.out.println("Hello world! " + nome);
+        System.out.println("He's year old " + idade);
     }
 }
